9d94b377fd12f348253370f0048567a2fe39db3e
Kelly Khalilinia <kellykhalilinia@Kellys-MacBook-Pro.local>
2023-06-05 17:55:12 -0700

    Feature 7 Complete

--- tracker/urls.py
+++ tracker/urls.py
@@ -19,8 +19,14 @@ from django.urls import path, include
 from django.shortcuts import redirect
 from projects.views import redirect_to_list_projects
 
+
+def redirect_to_list_projects(request):
+    return redirect("list_projects")
+
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("projects/", include("projects.urls")),
     path("", redirect_to_list_projects, name="home"),
+    path("accounts/", include("accounts.urls")),
 ]
